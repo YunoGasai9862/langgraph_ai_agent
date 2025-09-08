617aa4d938df92b037d9eff5de6b16849c397fb5
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-09-08 03:10:06 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from typing import Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+load_dotenv(verbose=True)
+
+llm = init_chat_model(
+    
+)
